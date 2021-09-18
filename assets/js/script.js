93462fe659894127c478a8f5752e26dd202a1702
curriecoder <andrew.j.currie0@gmail.com>
2021-09-18 18:37:38 -0400

    made connection to weather API for input city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,39 @@
 var APIKey = "66547e9042ab360b2c2f1ef36d0552f5";
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
+var searchButton = $("#searchBtn");
 
 // returns city name from input field and stores it as city.
-$("#searchBtn").on("click", function(){
-  city = $("input").val()
+
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
+  var city = $("input").val();
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  var requestUrl = queryURL;
   console.log(city);
-});
 
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      //Loop over the data to generate a table, each table row will have a link to the repo url
+      // for (var i = 0; i < data.length; i++) {
+      //   // Creating elements, tablerow, tabledata, and anchor
+      //   var createTableRow = document.createElement('tr');
+      //   var tableData = document.createElement('td');
+      //   var link = document.createElement('a');
 
+      //   // Setting the text of link and the href of the link
+      //   link.textContent = data[i].html_url;
+      //   link.href = data[i].html_url;
 
+      //   // Appending the link to the tabledata and then appending the tabledata to the tablerow
+      //   // The tablerow then gets appended to the tablebody
+      //   tableData.appendChild(link);
+      //   createTableRow.appendChild(tableData);
+      //   tableBody.appendChild(createTableRow);
+      // }
+    });
+}
+
+searchButton.on('click', getApi);
