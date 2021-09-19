98c737b9a4036246d62305a8a72ddcbed50d9764
curriecoder <andrew.j.currie0@gmail.com>
2021-09-18 21:00:09 -0400

    added temp, wind, humidity and created additional fetch for uv index data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,57 @@
 var APIKey = "66547e9042ab360b2c2f1ef36d0552f5";
 var searchButton = $("#searchBtn");
 
-// returns city name from input field and stores it as city.
-
+// returns city name from input field and makes request for weather.
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
   var city = $("input").val();
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-  var requestUrl = queryURL;
+  var requestUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=" +
+    APIKey;
   console.log(city);
-
+  var cityName = $("#cityNameDate");
+  var today = moment().format("M/D/YYYY");
+  
+  cityName.text(city + " " + "(" + today + ")");
+  
   fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    
+    var temp = $("#temp");
+    var wind = $("#wind");
+    var humidity = $("#humidity");
+    var uvIndex = $("#uvIndex");
+    var lat = data.coord.lat;
+    var lon =data.coord.lon;
+    
+    temp.text("Temp: " + data.main.temp + "°F");
+    wind.text("Wind: " + data.wind.speed + " mph");
+    humidity.text("Humidity: " + data.main.humidity + " %");
+    
+    var requestUrlUV =
+    "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=hourly,minutely,daily,alerts&appid=" +
+    APIKey;
+    
+    fetch(requestUrlUV)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      // for (var i = 0; i < data.length; i++) {
-      //   // Creating elements, tablerow, tabledata, and anchor
-      //   var createTableRow = document.createElement('tr');
-      //   var tableData = document.createElement('td');
-      //   var link = document.createElement('a');
-
-      //   // Setting the text of link and the href of the link
-      //   link.textContent = data[i].html_url;
-      //   link.href = data[i].html_url;
-
-      //   // Appending the link to the tabledata and then appending the tabledata to the tablerow
-      //   // The tablerow then gets appended to the tablebody
-      //   tableData.appendChild(link);
-      //   createTableRow.appendChild(tableData);
-      //   tableBody.appendChild(createTableRow);
-      // }
-    });
+      console.log(data);
+      uvIndex.text("UV Index: " + data.current.uvi);
+        });
+  });
+  
 }
 
-searchButton.on('click', getApi);
+searchButton.on("click", getApi);
