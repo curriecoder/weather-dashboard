6c4e01e3fb9400cc0abcd49edf66cec8c8a4c4c6
curriecoder <andrew.j.currie0@gmail.com>
2021-09-19 21:06:37 -0400

    made contact for 5 day weather forecast, icons not currently playing ball

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ function getApi() {
   var today = moment().format("M/D/YYYY");
 
   cityName.text(city + " " + "(" + today + ")");
-
+  console.log(city);
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -30,8 +30,8 @@ function getApi() {
       var lon = data.coord.lon;
 
       temp.text("Temp: " + data.main.temp + "°F");
-      wind.text("Wind: " + data.wind.speed + " mph");
-      humidity.text("Humidity: " + data.main.humidity + " %");
+      wind.text("Wind: " + data.wind.speed + "mph");
+      humidity.text("Humidity: " + data.main.humidity + "%");
 
       var requestUrlUV =
         "http://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -49,23 +49,46 @@ function getApi() {
           uvIndex.text("UV Index: " + data.current.uvi);
 
           var uviColor = data.current.uvi;
-          console.log(uviColor);
           if (uviColor < 2) {
-            uvIndex.attr("class", "bg-success");
+            uvIndex.attr("class", "bg-success text-white");
           } else if (3 < uviColor < 5) {
-            uvIndex.attr("class", "bg-warning");
+            uvIndex.attr("class", "bg-warning text-white");
           } else if (6 < uviColor < 7) {
-            uvIndex.attr("class", "bg-orange");
+            uvIndex.attr("class", "bg-orange text-white");
           } else if (8 < uviColor < 10) {
-            uvIndex.attr("class", "bg-danger");
+            uvIndex.attr("class", "bg-danger text-white");
           } else if (uviColor > 10) {
-            uvIndex.attr("class", "bg-violet");
+            uvIndex.attr("class", "bg-violet text-white");
           }
-          
-          
+        });
+
+      var requestUrl5Day =
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&cnt=5&units=imperial&appid=" +
+        APIKey;
+
+      fetch(requestUrl5Day)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          var wf = ""; // start here for refactoring
+          wf += data.city.name; // City (displays once)
+          $.each(data.list, function (index, val) {
+            wf += " Day " + index + ","; // Day
+            wf += " Temperature: " + val.main.temp + " °F,"; // Temperature
+            wf += " Weather " + val.weather[0].description + ", "; // Description
+            wf +=
+              '<img src= "https://openweathermap.org/img/wn/' + //good link format
+              val.weather[0].icon +
+              '@2x.png">'; // Icon
+            console.log(wf);
+            $('#forecast').text(wf);
+          });
         });
     });
 }
 
 searchButton.on("click", getApi);
-
